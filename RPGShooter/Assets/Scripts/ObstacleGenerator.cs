dc1815f1957f4b46cdc53753be45474223e55a32
KentCud1 <kentcudi@gmail.com>
2019-08-09 17:31:49 -0400

    lL
    l

---
+++ RPGShooter/Assets/Scripts/ObstacleGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleGenerator : MonoBehaviour
+{
+    public Vector2 obstacleMaxRect;
+    public Bounds obstacleArea;
+    public float gapLength;
+    public int maxObstacles;
+    public int currentObstacles;
+    public GameObject obstaclePrefab;
+
+    public LayerMask laymask;
+    // Use this for initialization
+    void Start() {
+        obstacleArea.extents = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.scaledPixelWidth, Camera.main.scaledPixelHeight, 10));
+    }
+
+    // Update is called once per frame
+    void Update() {
+        currentObstacles = FindObjectsOfType<ObstacleMovement>().Length;
+        if (currentObstacles < maxObstacles)
+        {
+            GameObject go = Instantiate(obstaclePrefab, new Vector3(
+                Random.Range(-obstacleArea.extents.x, obstacleArea.extents.x), obstacleArea.extents.y, 0),
+                Quaternion.identity, transform);
+            go.transform.localScale = new Vector2(Random.Range(1, obstacleMaxRect.x), Random.Range(1, obstacleMaxRect.y));
+
+            Bounds bound = go.GetComponent<BoxCollider2D>().bounds;
+
+             float cornerLen = Mathf.Sqrt(bound.size.x * bound.size.x +
+                bound.size.y * bound.size.y);
+            Debug.Log(cornerLen + "\n" + bound.size);
+
+            RaycastHit2D hit = Physics2D.CircleCast(go.transform.position, cornerLen + gapLength, Vector2.zero, 5f, laymask);
+            if(hit.transform != null && hit.transform != go.transform)
+            {
+                Debug.Log("Overlap");
+                Destroy(go);
+            }
+        }
+
+    }
+
+}
+
---
+++ RPGShooter/Assets/Scripts/ObstacleMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleMovement : MonoBehaviour {
+    public Vector2 direction;
+    public float speed;
+
+    float lowerEdge;
+    float upperSpriteEdge;
+
+
+	// Use this for initialization
+	void Start () {
+        lowerEdge = -Camera.main.ScreenToWorldPoint(new Vector3(0, Camera.main.scaledPixelHeight, 0)).y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(direction.normalized * speed * Time.deltaTime);
+
+        upperSpriteEdge = transform.position.y + GetComponent<SpriteRenderer>().bounds.extents.y;
+        if (upperSpriteEdge < lowerEdge)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
