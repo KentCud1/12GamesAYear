466f84a49eac70bb011a10c19ae732cef09bd9ee
KentCud1 <kentcudi@gmail.com>
2020-06-02 15:51:50 -0400

    WIP

---
+++ WIP(/Assets/Scripts/Notes/NotesOnCharacters.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+///////////////////////////////////////////////////////Character notes/////////////////////////////////////////////////////////
+/* Characters have health, attack, defense, and speed.
+ * Characters run in groups.
+ * Groups contain one type of character.
+ * Groups where the characters have a higher speed will attack first.
+ * The whole group will attack per turn.
+ * Characters will roll attack between lower an upper bounds displayed on "card" (e.g 2-4 = 2,3, or 4).
+ * Attack roll will be contested by the lowest number of the defending characters defense, the higher value wins. (e.g roll of 3 vs. 2-4 def(2), 3 wins for attack).
+ * The 
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     */
